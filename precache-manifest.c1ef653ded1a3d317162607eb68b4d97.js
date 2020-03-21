20da9833e079f9e5ea4512b6d0ce5e60789261bc
Eric Wang <wdq1103@163.com>
2020-03-21 16:39:20 +0800

    Add files via upload

---
+++ precache-manifest.c1ef653ded1a3d317162607eb68b4d97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c19f1a6ce7d7f490310295af14ab861",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90841e5861d43b3bc1ed",
+    "url": "/static/css/main.78186613.css"
+  },
+  {
+    "revision": "2bb908f9eb37a53eb5b4",
+    "url": "/static/js/fragment.c4d244b8.js"
+  },
+  {
+    "revision": "90841e5861d43b3bc1ed",
+    "url": "/static/js/main.ac43c6be.js"
+  },
+  {
+    "revision": "16808a380c62e47aced549a449dc15e7",
+    "url": "/static/js/main.ac43c6be.js.LICENSE.txt"
+  }
+]);
